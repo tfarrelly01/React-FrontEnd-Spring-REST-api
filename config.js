09a49e53c02f7149ef9bb20b293ea7bbeb4d6f72
Tony Farrelly <tfarrelly01@gmail.com>
2017-08-24 12:11:32 +0100

    Corrected error in config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 
 module.exports = {
  // ROUTE: 'http://localhost:8081'
-  ROUTE: 'http://ec2-54-77-211-242.eu-west-1.compute.amazonaws.com:80'
+  ROUTE: 'http://ec2-54-77-211-242.eu-west-1.compute.amazonaws.com:8081'
 };
 /*
 axios.get('http://localhost:8080/buildings')
